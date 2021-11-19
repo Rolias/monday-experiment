6ef144049371a390ecec559003ef800a8c3a1b90
Tod Gentille <tod.gentille@gmail.com>
2021-11-19 14:16:58 -0800

    initial commit

---
+++ test/.mocharc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  "bail":true,
+  "verbose": true,
+  "reporter": "spec",
+  "timeout": 5000,
+  "require": [
+    "ts-node/register",
+    "source-map-support/register",
+    "chai",
+    "sinon",
+    "sinon-chai",
+    "chai-subset",
+    `chai-as-promised`,
+    'chai/register-should',
+    `test/mocha-init`
+    ],
+    recursive: true,
+}
